820e21f1c57f3aec8f60d0c9b73a7d8fc894b05b
luwh0708 <luwh0708@thundersoft.com>
2020-11-12 18:30:38 +0800

    FIX: algorithm

--- src/modules/algorithm.cpp
+++ src/modules/algorithm.cpp
@@ -1,3 +1,10 @@
+// #define ALGORITHM_DEBUG_MEMSET
+// #define ALGORITHM_DEBUG_DIST
+// #define ALGORITHM_DEBUG_DIST_GRAPH
+// #define ALGORITHM_DEBUG_MOVE_VAL
+// #define ALGORITHM_DEBUG_ADD_VAL
+// #define ALGORITHM_DEBUG_CHAR_GRAPH
+// #define ALGORITHM_DEBUG_ALGORITHM
 #include "modules/algorithm.h"
 #include <stdio.h>
 #include <queue>
@@ -6,13 +13,12 @@
 
 int evaluate(class server_data &m, double &score)
 {
-	// 周围围一圈墙
 	int line_size = m.get_map_size();
-	int map_size = line_size * line_size + 4 * line_size + 4;
+	int map_size = line_size * line_size;
 
 	// 定义位置函数
 	auto get_pos = [&](int row, int col) -> int {
-		return (row + 1) * (line_size + 2) + col + 1;
+		return row * line_size + col;
 	};
 
 	// 定义堆
@@ -37,7 +43,11 @@ int evaluate(class server_data &m, double &score)
 
 	// 计算距离图
 	int *dist_graph = new int[map_size];
+#ifdef ALGORITHM_DEBUG_MEMSET
+	for (int i = 0; i < map_size; i++) dist_graph[i] = 999;
+#else
 	memset(dist_graph, 0x7f, map_size * sizeof(int));
+#endif
 	auto get_dist = [&](int row, int col) -> int {
 		if (get_pos(row, col) >= map_size) printf("get dist %d\n", get_pos(row, col));
 		if (get_pos(row, col) < 0) printf("get dist %d\n", get_pos(row, col));
@@ -46,6 +56,8 @@ int evaluate(class server_data &m, double &score)
 	auto set_dist = [&](int row, int col, int dist) -> void {
 		int pos = get_pos(row, col);
 		if (pos < 0 || pos >= map_size) {
+			printf("set_dist %d, %d = %d\n", row, col, dist);
+			printf("set dist %d\n", get_pos(row, col));
 			return;
 		}
 		dist_graph[get_pos(row, col)] = dist;
@@ -62,10 +74,13 @@ int evaluate(class server_data &m, double &score)
 			// 目标行列
 			int tr = node.row + (dir == 's') - (dir == 'w');
 			int tc = node.col + (dir == 'd') - (dir == 'a');
+#ifdef ALGORITHM_DEBUG_DIST
+			printf("target: %d, %d\n", tr, tc);
+#endif
 			// 不能是墙
 			bool have_value = m.get(tr, tc) != '9';
 			// 距离要更优
-			int move_dist = node.dist + 1 + (node.dir == dir);
+			int move_dist = node.dist + 1 + (node.dir != dir);
 			if (tr < 0 || tr >= line_size) {
 				return;
 			}
@@ -73,6 +88,9 @@ int evaluate(class server_data &m, double &score)
 				return;
 			}
 			have_value = have_value && move_dist <= get_dist(tr, tc);
+#ifdef ALGORITHM_DEBUG_DIST
+			printf("have: %d\n", have_value);
+#endif
 			if (have_value) {
 				pq.push(qnode(tr, tc, dir, move_dist));
 			}
@@ -84,13 +102,28 @@ int evaluate(class server_data &m, double &score)
 		check_move('d');
 	}
 
+#ifdef ALGORITHM_DEBUG_DIST_GRAPH
+	printf("dist_graph:\n");
+	for (int i = 0; i < line_size; i++) {
+		for (int j = 0; j < line_size; j++) {
+			printf("%3d, ", dist_graph[i * line_size + j]);
+		}
+		printf("\n");
+	}
+#endif
+
 	// 计算移动价值图
 	double *move_val = new double[map_size];
 	memset(move_val, 0, map_size * sizeof(double));
 	auto get_move_val = [&](int row, int col) -> double {
+		if (get_pos(row, col) >= map_size) printf("get move %d\n", get_pos(row, col));
+		if (get_pos(row, col) < 0) printf("get move %d\n", get_pos(row, col));
 		return move_val[get_pos(row, col)];
 	};
 	auto add_move_val = [&](int row, int col, double val) -> void {
+#ifdef ALGORITHM_DEBUG_ADD_VAL
+		printf("set move (%d, %d) += %lf\n", row, col, val);
+#endif
 		int pos = get_pos(row, col);
 		if (pos < 0 || pos >= map_size) {
 			return;
@@ -106,7 +139,7 @@ int evaluate(class server_data &m, double &score)
 			char ch = m.get(i, j);
 			switch (ch) {
 				case '1': case '2': case '3': case '4': case '5':
-					add_move_val(i, j, ch - '0');
+					add_move_val(i, j, (ch - '0') * 1.0 / get_dist(i, j));
 					break;
 
 				case 'w': case 'a': case 's': case 'd':
@@ -165,11 +198,31 @@ int evaluate(class server_data &m, double &score)
 		}
 	}
 
+#ifdef ALGORITHM_DEBUG_MOVE_VAL
+	printf("move_val:\n");
+	for (int i = 0; i < line_size; i++) {
+		for (int j = 0; j < line_size; j++) {
+			printf("%.2lf\t", move_val[i * line_size + j]);
+		}
+		printf("\n");
+	}
+#endif
+
+#ifdef ALGORITHM_DEBUG_CHAR_GRAPH
+	printf("char_graph:\n");
+	for (int i = 0; i < line_size; i++) {
+		for (int j = 0; j < line_size; j++) {
+			printf("%c ", m.get(i, j));
+		}
+		printf("\n");
+	}
+#endif
+
 	score = 0;
 	for (int i = 0; i < line_size; i++) {
 		for (int j = 0; j < line_size; j++) {
 			if (get_dist(i, j)) {
-				score += get_move_val(i, j) / get_dist(i, j);
+				score += get_move_val(i, j);
 			}
 		}
 	}
@@ -195,8 +248,14 @@ std::pair<enum move_operating, bool> algorithm(class server_data &m)
 		class server_data cloned = m.clone();
 		ret = cloned.move(_move);
 		if (ret == 0) {
+#ifdef ALGORITHM_DEBUG_ALGORITHM
+			printf("%d, 0\n", _move);
+#endif
 			evaluate(cloned, situation_point);
 			this_score = cloned.get_my_score() - my_score;
+#ifdef ALGORITHM_DEBUG_ALGORITHM
+			printf("score: %d, point: %lf\n\n", this_score, situation_point);
+#endif
 			if (max_score < this_score) {
 				max_score = cloned.get_my_score() - my_score;
 				max_situation_point = situation_point;
@@ -207,21 +266,27 @@ std::pair<enum move_operating, bool> algorithm(class server_data &m)
 				move = _move;
 				fire = false;
 			}
-		}
 
-		ret = cloned.fire();
-		if (ret == 0) {
-			evaluate(cloned, situation_point);
-			this_score = cloned.get_my_score() - my_score;
-			if (max_score < this_score) {
-				max_score = cloned.get_my_score() - my_score;
-				max_situation_point = situation_point;
-				move = _move;
-				fire = true;
-			} else if (max_score == this_score && max_situation_point < situation_point) {
-				max_situation_point = situation_point;
-				move = _move;
-				fire = true;
+			ret = cloned.fire();
+			if (ret == 0) {
+#ifdef ALGORITHM_DEBUG_ALGORITHM
+				printf("%d, 1\n", _move);
+#endif
+				evaluate(cloned, situation_point);
+				this_score = cloned.get_my_score() - my_score;
+#ifdef ALGORITHM_DEBUG_ALGORITHM
+				printf("score: %d, point: %lf\n\n", this_score, situation_point);
+#endif
+				if (max_score < this_score) {
+					max_score = cloned.get_my_score() - my_score;
+					max_situation_point = situation_point;
+					move = _move;
+					fire = true;
+				} else if (max_score == this_score && max_situation_point < situation_point) {
+					max_situation_point = situation_point;
+					move = _move;
+					fire = true;
+				}
 			}
 		}
 	};
--- test/4.algorithm/2.algorithm.cpp
+++ test/4.algorithm/2.algorithm.cpp
@@ -1,22 +1,45 @@
 #include "modules/algorithm.h"
 #include "utils/assert.h"
 
+#define test(map, move, is_fire) \
+{\
+	class server_data data((char *)map, 5, 0);\
+	auto result = algorithm(data);\
+	/*printf("%d, %d\n", result.first, result.second);*/ \
+	assert(result.first == move);\
+	assert(result.second == is_fire);\
+}
+
 int main()
 {
-	char map[] =
-		"[MAP A@ "
+	test("[MAP A@ "
 		"12345"
-		"0sdwg"
+		"0sdwG"
 		"99999"
 		"54321"
 		"w2345"
-		"][LOCATION A@ 20 6 7 8][SCORE 10 1 2 3]";
-
-	class server_data data(map, 5, 0);
-
-	auto result = algorithm(data);
-	assert(result.first == move_op_up);
-	assert(result.second == false);
+		"][LOCATION A@ 20 6 7 8][SCORE 10 1 2 3]" , move_op_up, false);
+	test("[MAP A@ "
+		"w3000"
+		"20000"
+		"00000"
+		"00000"
+		"0000G"
+		"][LOCATION A@ 0][SCORE 10]" , move_op_right, false);
+	test("[MAP A@ "
+		"19441"
+		"3s214"
+		"40312"
+		"10213"
+		"40325"
+		"][LOCATION A@ 6][SCORE 10]" , move_op_right, false);
+	test("[MAP A@ "
+		"G4440"
+		"09590"
+		"02G40"
+		"00040"
+		"1322s"
+		"][LOCATION A@ 24][SCORE 10]" , move_op_left, false);
 
 	return 0;
 }
