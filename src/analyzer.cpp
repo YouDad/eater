9676c05951b0d7e0aac19ea48c29c0d9fad1eabc
luwh0708 <luwh0708@thundersoft.com>
2020-11-13 14:39:49 +0800

    ADD: analyzer

---
+++ src/analyzer.cpp
@@ -0,0 +1,47 @@
+#include "modules/draw.h"
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <string.h>
+
+const int map_size = 25;
+int ptr = 0;
+int main()
+{
+	auto fp = fopen(".record", "r");
+
+	std::vector<std::string> strings;
+	while (!feof(fp)) {
+		char buf[map_size * map_size + 100];
+		fgets(buf, sizeof(buf), fp);
+		strings.push_back(buf);
+	}
+
+	fclose(fp);
+
+	for (int i = 0; i < map_size; i++) {
+		printf("\n");
+	}
+
+	while (!feof(stdin)) {
+		char buf[16];
+		fgets(buf, 16, stdin);
+		printf("\033[2J");
+		if (buf[0] != '\n') {
+			if (buf[0] == 'p') {
+				printf("%s\n", strings[ptr - 1].c_str());
+				continue;
+			} else {
+				ptr = atoi(buf);
+			}
+		}
+
+		printf("\033[1Around %d:\n\033[1A", ptr);
+		draw((char *)strings[ptr++].c_str(), map_size, 0);
+		if (ptr == strings.size()) {
+			ptr--;
+		}
+	}
+
+	return 0;
+}
